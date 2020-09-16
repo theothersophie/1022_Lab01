b97ab3a7a6ca48ff33e7052341e0bb402e644e3b
sophie <sophia97@my.yorku.ca>
2020-09-16 13:32:38 -0400

    created class

---
+++ app/src/main/java/ca/yorku/a1022_lab01/BMIModel.java
@@ -0,0 +1,14 @@
+package ca.yorku.a1022_lab01;
+
+public class BMIModel
+{
+    public static double toDouble(String s) {
+
+    }
+    public static double getBMI(double weight, double height) {
+
+    }
+    public static String formatBMI(double bmi) {
+
+    }
+}
