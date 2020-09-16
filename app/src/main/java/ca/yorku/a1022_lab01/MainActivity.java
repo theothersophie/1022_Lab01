efa3bee0b0070f1416cfdfd96336e27918cd5c07
EECS User <user@wsc108-11.eecs.yorku.ca>
2020-09-16 13:19:58 -0400

    Initial commit

---
+++ app/src/main/java/ca/yorku/a1022_lab01/MainActivity.java
@@ -0,0 +1,16 @@
+package ca.yorku.a1022_lab01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
